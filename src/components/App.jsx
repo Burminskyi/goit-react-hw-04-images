92a70d93f0bf7d3590c8efaf1a5929780ef6b506
Burminskyi <119361840+Burminskyi@users.noreply.github.com>
2023-07-06 01:00:58 +0300

    changed class App to the function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 
 import { ImageGallery } from './ImageGallery/ImageGallery';
@@ -7,95 +7,78 @@ import { Loader } from './Loader/Loader';
 
 import { getPhotos } from 'services/getPhotos';
 
-export class App extends Component {
-  state = {
-    searchQuery: '',
-    pictures: [],
-    status: 'idle',
-    page: 1,
-    totalImages: 0,
-    error: '',
-  };
+export const App = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [pictures, setPictures] = useState([]);
+  const [status, setStatus] = useState('idle');
+  const [page, setPage] = useState(1);
+  const [totalImages, setTotalImages] = useState(0);
+  const [error, setError] = useState('');
 
-  async componentDidUpdate(prevProps, prevState) {
-    const { page, searchQuery } = this.state;
-
-    if (searchQuery !== prevState.searchQuery || page !== prevState.page) {
-      this.setState({ status: 'pending' });
+  useEffect(() => {
+    if (!searchQuery) return;
+    const updateComponent = async () => {
+      setStatus('pending');
       try {
         const data = await getPhotos(searchQuery, page);
         if (!data.totalHits) {
-          this.setState({
-            status: 'rejected',
-            error: 'За даним запитом немає зображень :(',
-          });
+          setStatus('rejected');
+          setError('За даним запитом немає зображень :(');
           return;
         }
 
-        this.setState(prevState => {
-          return {
-            pictures: [...prevState.pictures, ...data.hits],
-            btnIsShown: false,
-            status: 'resolved',
-            totalImages: data.totalHits,
-          };
-        });
+        setPictures(prevPictures => [...prevPictures, ...data.hits]);
+        setStatus('resolved');
+        setTotalImages(data.totalHits);
       } catch (error) {
-        this.setState({
-          status: 'rejected',
-          error: error.message,
-        });
+        setStatus('rejected');
+        setError(error.message);
       }
-    }
-  }
+    };
+    updateComponent();
+  }, [page, searchQuery]);
 
-  handleSearchSubmit = searchQuery => {
-    this.setState({
-      page: 1,
-      searchQuery,
-      pictures: [],
-      totalImages: 0,
-    });
+  const handleSearchSubmit = searchQuery => {
+    setPage(1);
+    setSearchQuery(searchQuery);
+    setPictures([]);
+    setTotalImages(0);
   };
 
-  handleLoadMoreClick = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const handleLoadMoreClick = () => {
+    setPage(page + 1);
   };
 
-  render() {
-    const { status, pictures, totalImages, error, page } = this.state;
-    const showBtn = totalImages !== pictures.length && page * 12 < 500;
-    if (status === 'idle') {
-      return (
-        <>
-          <Searchbar onSubmit={this.handleSearchSubmit} />
-        </>
-      );
-    }
-    if (status === 'pending')
-      return (
-        <>
-          <Searchbar onSubmit={this.handleSearchSubmit} />
-          {pictures.length > 0 && <ImageGallery pictures={pictures} />}
-          <Loader />
-        </>
-      );
-    if (status === 'rejected')
-      return (
-        <>
-          <Searchbar onSubmit={this.handleSearchSubmit} />
-          <p>{error}</p>
-        </>
-      );
-    if (status === 'resolved')
-      return (
-        <>
-          <Searchbar onSubmit={this.handleSearchSubmit} />
-          <ImageGallery pictures={pictures} />
-          {showBtn && <Button onClick={this.handleLoadMoreClick} />}
-        </>
-      );
+  const showBtn = totalImages !== pictures.length && page * 12 < 500;
+
+  if (status === 'idle') {
+    return (
+      <>
+        <Searchbar onSubmit={handleSearchSubmit} />
+      </>
+    );
   }
-}
+  if (status === 'pending')
+    return (
+      <>
+        <Searchbar onSubmit={handleSearchSubmit} />
+        {pictures.length > 0 && <ImageGallery pictures={pictures} />}
+        <Loader />
+      </>
+    );
+  if (status === 'rejected')
+    return (
+      <>
+        <Searchbar onSubmit={handleSearchSubmit} />
+        <p>{error}</p>
+      </>
+    );
+  if (status === 'resolved')
+    return (
+      <>
+        <Searchbar onSubmit={handleSearchSubmit} />
+        <ImageGallery pictures={pictures} />
+        {showBtn && <Button onClick={handleLoadMoreClick} />}
+      </>
+    );
+};
